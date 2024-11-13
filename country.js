01f594307c0cacb42babc8abeac17620f8a9fb7c
Anshika Gupta <132778691+anshika-gupta-98@users.noreply.github.com>
2024-11-13 16:08:48 +0530

    Added country's list

---
+++ country.js
@@ -0,0 +1,127 @@
+// countryList.js
+const countryList = {
+    AED: "AE",  // United Arab Emirates
+    AFN: "AF",  // Afghanistan
+    ALL: "AL",  // Albania
+    AMD: "AM",  // Armenia
+    ANG: "AN",  // Netherlands Antilles
+    AOA: "AO",  // Angola
+    ARS: "AR",  // Argentina
+    AUD: "AU",  // Australia
+    AZN: "AZ",  // Azerbaijan
+    BAM: "BA",  // Bosnia and Herzegovina
+    BBD: "BB",  // Barbados
+    BDT: "BD",  // Bangladesh
+    BGN: "BG",  // Bulgaria
+    BHD: "BH",  // Bahrain
+    BIF: "BI",  // Burundi
+    BMD: "BM",  // Bermuda
+    BND: "BN",  // Brunei
+    BOB: "BO",  // Bolivia
+    BRL: "BR",  // Brazil
+    BSD: "BS",  // Bahamas
+    CAD: "CA",  // Canada
+    CDF: "CD",  // Democratic Republic of the Congo
+    CHF: "CH",  // Switzerland
+    CLP: "CL",  // Chile
+    CNY: "CN",  // China
+    COP: "CO",  // Colombia
+    CRC: "CR",  // Costa Rica
+    CUP: "CU",  // Cuba
+    CVE: "CV",  // Cape Verde
+    CYP: "CY",  // Cyprus
+    CZK: "CZ",  // Czech Republic
+    DKK: "DK",  // Denmark
+    DOP: "DO",  // Dominican Republic
+    DZD: "DZ",  // Algeria
+    EGP: "EG",  // Egypt
+    EUR: "EU",  // Euro (General)
+    FJD: "FJ",  // Fiji
+    GBP: "GB",  // United Kingdom
+    GEL: "GE",  // Georgia
+    GHS: "GH",  // Ghana
+    GIP: "GI",  // Gibraltar
+    GMD: "GM",  // Gambia
+    GNF: "GN",  // Guinea
+    GTQ: "GT",  // Guatemala
+    GYD: "GY",  // Guyana
+    HKD: "HK",  // Hong Kong
+    HNL: "HN",  // Honduras
+    HRK: "HR",  // Croatia
+    HUF: "HU",  // Hungary
+    IDR: "ID",  // Indonesia
+    ILS: "IL",  // Israel
+    INR: "IN",  // India
+    IQD: "IQ",  // Iraq
+    IRR: "IR",  // Iran
+    ISK: "IS",  // Iceland
+    JMD: "JM",  // Jamaica
+    JPY: "JP",  // Japan
+    KES: "KE",  // Kenya
+    KGS: "KG",  // Kyrgyzstan
+    KHR: "KH",  // Cambodia
+    KMF: "KM",  // Comoros
+    KPW: "KP",  // North Korea
+    KRW: "KR",  // South Korea
+    KWD: "KW",  // Kuwait
+    KYD: "KY",  // Cayman Islands
+    KZT: "KZ",  // Kazakhstan
+    LAK: "LA",  // Laos
+    LBP: "LB",  // Lebanon
+    LKR: "LK",  // Sri Lanka
+    LRD: "LR",  // Liberia
+    LSL: "LS",  // Lesotho
+    LTL: "LT",  // Lithuania
+    LVL: "LV",  // Latvia
+    LYD: "LY",  // Libya
+    MAD: "MA",  // Morocco
+    MDL: "MD",  // Moldova
+    MGA: "MG",  // Madagascar
+    MKD: "MK",  // North Macedonia
+    MMK: "MM",  // Myanmar
+    MNT: "MN",  // Mongolia
+    MOP: "MO",  // Macau
+    MUR: "MU",  // Mauritius
+    MVR: "MV",  // Maldives
+    MWK: "MW",  // Malawi
+    MXN: "MX",  // Mexico
+    MYR: "MY",  // Malaysia
+    MZN: "MZ",  // Mozambique
+    NAD: "NA",  // Namibia
+    NGN: "NG",  // Nigeria
+    NIO: "NI",  // Nicaragua
+    NPR: "NP",  // Nepal
+    NZD: "NZ",  // New Zealand
+    OMR: "OM",  // Oman
+    PAB: "PA",  // Panama
+    PEN: "PE",  // Peru
+    PHP: "PH",  // Philippines
+    PKR: "PK",  // Pakistan
+    PLN: "PL",  // Poland
+    PYG: "PY",  // Paraguay
+    QAR: "QA",  // Qatar
+    RON: "RO",  // Romania
+    RUB: "RU",  // Russia
+    SAR: "SA",  // Saudi Arabia
+    SBD: "SB",  // Solomon Islands
+    SCR: "SC",  // Seychelles
+    SEK: "SE",  // Sweden
+    SGD: "SG",  // Singapore
+    SLL: "SL",  // Sierra Leone
+    SYP: "SY",  // Syria
+    THB: "TH",  // Thailand
+    TJS: "TJ",  // Tajikistan
+    TMT: "TM",  // Turkmenistan
+    TND: "TN",  // Tunisia
+    TRY: "TR",  // Turkey
+    TTD: "TT",  // Trinidad and Tobago
+    TWD: "TW",  // Taiwan
+    TZS: "TZ",  // Tanzania
+    UAH: "UA",  // Ukraine
+    UGX: "UG",  // Uganda
+    USD: "US",  // United States
+    UYU: "UY",  // Uruguay
+    VND: "VN",  // Vietnam
+    ZAR: "ZA"   // South Africa
+  };
+  
